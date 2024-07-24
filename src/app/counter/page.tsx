e929cf7bd825639304b3a275e8726a0d70611a2a
kevinZzzzzzz <jie736805161@qq.com>
2024-07-24 17:30:24 +0800

    save

--- src/app/counter/page.tsx
+++ src/app/counter/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"; // 声明该组件为客户端组件
 import { useState } from "react";
 
 function Counter(props: any) {
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,33 @@
 import styles from "./index.module.scss";
 
-export default function HomePage() {
-  return <h1 style={{ color: styles.primaryColor }}>HomePage</h1>;
+// 使用 generateMetadata 函数动态生成元数据
+export async function generateMetadata() {
+  return {
+    title: "首页",
+    description: "首页的描述",
+  };
+}
+
+// 模拟数据
+async function getData() {
+  const result = await fetch(process.env.URL + "/api/avatar-list", {
+    method: "GET",
+  });
+  if (result.ok) {
+    return result.json();
+  }
+  return [];
+}
+export default async function HomePage() {
+  const res = await getData();
+  return (
+    <>
+      <h1 style={{ color: styles.primaryColor }}>HomePage</h1>
+      <ul>
+        {res.map((d: { title: string }, idx: number) => {
+          return <li key={idx}>{d.title}</li>;
+        })}
+      </ul>
+    </>
+  );
 }
